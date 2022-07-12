5f6dd165977ecf3165f169018a68126a457b4383
Juyeong Song <80014509+sjy1410@users.noreply.github.com>
2022-07-13 08:36:24 +0900

    pass문의 활용 (문자열 최종보스)

--- 단계별로 풀어보기/문자열/1316 그룹 단어 체커.py
+++ 단계별로 풀어보기/문자열/1316 그룹 단어 체커.py
@@ -3,7 +3,7 @@ cnt = n #일단 입력 될 단어수만큼 그룹단어수 설정
 
 for i in range(n):
     s = input()
-    for j in range(0, len(s)-1): #배열이니까 0번지 부터
+    for j in range(0, len(s)-1): #배열이니까 0번지 부터(단어길이만큼 반복)
         if s[j] == s[j+1]: #문자 두 개 이상 연속해서 나타나면
             pass #for문 계속 실행
         elif s[j] in s[j+1:]: #문자가 연속되지 않으면(한 문자가 다다음 문자들의 배열에 속해있으면)
